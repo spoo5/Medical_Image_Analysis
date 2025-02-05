e5aaed133c219db80e1cccb9f2a64832415dc91c
spoo5 <134118809+spoo5@users.noreply.github.com>
2025-02-05 08:18:48 +0530

    Update bone_fracture_model.py
    inital commit

--- bone_fracture_model.py
+++ bone_fracture_model.py
@@ -4,23 +4,17 @@ from tensorflow.keras.applications import ResNet50
 from tensorflow.keras.layers import Dense, GlobalAveragePooling2D
 from tensorflow.keras.models import Model
 
-# Load the ResNet50 model with pre-trained weights (exclude the top layer)
 base_model = ResNet50(weights='imagenet', include_top=False, input_shape=(224, 224, 3))
 
-# Freeze the base model layers to avoid retraining them
 base_model.trainable = False
 
-# Add custom layers on top of the base model
 x = base_model.output
-x = GlobalAveragePooling2D()(x)  # Global average pooling layer
-x = Dense(1024, activation='relu')(x)  # Fully connected layer
-predictions = Dense(1, activation='sigmoid')(x)  # Output layer for binary classification
+x = GlobalAveragePooling2D()(x) 
+x = Dense(1024, activation='relu')(x)  
+predictions = Dense(1, activation='sigmoid')(x) 
 
-# Combine the base model and custom layers
 model = Model(inputs=base_model.input, outputs=predictions)
 
-# Compile the model
 model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
 
-# Print the model summary
 model.summary()
